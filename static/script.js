90ea862c40bd8c269d2ade4a3fce854a296e6237
suhas@20 <suhasshu00@gmail.com>
2024-08-28 22:50:07 +0530

    retry

--- static/script.js
+++ static/script.js
@@ -105,4 +105,14 @@ $(document).ready(function() {
   });
 });
 
+//Search funtionality in admin side.
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
   
